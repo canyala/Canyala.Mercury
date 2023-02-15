89a25a49318a7a43e8546664dad73831fecd2ee3
Martin Fredriksson <martin.fredriksson@canyala.se>
2023-02-15 11:18:44 +0100

    Update copyright notice.

--- Canyala.Mercury.Core/Uri.cs
+++ Canyala.Mercury.Core/Uri.cs
@@ -2,7 +2,7 @@
 
   MIT License
  
-  Copyright (c) 2022 Canyala Innovation (Martin Fredriksson)
+  Copyright (c) 2011-2023 Canyala Innovation (Martin Fredriksson)
 
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
